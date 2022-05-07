c45bb70760a4e8521a377cbbceaed25b98801ddd
imubarak234 <imubarak2424@gmail.com>
2022-05-07 18:27:26 +0100

    validations

--- app/models/class.rb
+++ app/models/class.rb
@@ -2,4 +2,7 @@ class Class < ApplicationRecord
   has_many :start_date
   has_many :classes_reservation
   has_many :classes_user
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
 
   has_many :classes_user
   has_many :reservation
+
+  validates :full_name, presence: true
 end
