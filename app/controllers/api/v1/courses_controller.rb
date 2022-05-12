2c0eaa4da0412d82dad608d4355adcc88238ac41
imubarak234 <imubarak2424@gmail.com>
2022-05-12 15:31:41 +0100

    payload

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,24 +1,26 @@
 module Api
   module V1
     class CoursesController < ApplicationController
-      ALLOWED_DATA = %(title, description, category, duration, photo, price).freeze
+      ALLOWED_DATA = %[title, description, category, duration, photo, price, user_id].freeze
 
       def index
         render json: Course.all
       end
 
       def create
+
         data = json_payload.select { |k| ALLOWED_DATA.include?(k) }
         courses = Course.new(data)
-        courses.update(user: current_user)
+        #courses.update(user: current_user)
 
         if course.save
           courses_users
           CoursesUser.create(course: courses, user: current_user)
-          render json: status: :created
+          render json: course, status: :created
         else
           render json: course.errors, status: :unproccessable_entity
         end
+
       end
 
       def destroy
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_default_format
+  protect_from_forgery with: :null_session
+  # include Knock::Authenticable
+  # undef_method :current_user
+
+  def json_payload
+    HashWithIndifferentAccess.new(JSON.parse(request.raw_post))
+  end
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
 end
