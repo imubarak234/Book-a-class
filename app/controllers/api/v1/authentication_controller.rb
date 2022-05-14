f4fdacabe25b41f04878d62d93d5db1cca4fd4cc
imubarak234 <imubarak2424@gmail.com>
2022-05-14 16:44:19 +0100

    jwt logic

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,18 +1,35 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
-    rescue_from ActionController::ParameterMissing, with: :paramter_missing
+      class AuthenticationError < StandardError; end
+
+      rescue_from ActionController::ParameterMissing, with: :paramter_missing
+      rescue_from AuthenticationError, with: :handle_unauthenticated 
 
       def create
-        params.require(:username).inspect
+        
         params.require(:password).inspect
+
+        raise AuthenticationError unless user.authenticate(params.require(:password))
+        tokens = AuthenticationTokenServices.call(user.id)
+
+        render json: { token: tokens }, status: :created
       end
 
       private
 
+      def user
+        @user ||= User.find_by(username: params.require(:username).inspect)
+      end
+
       def paramter_missing(e)
         render json: { error: e.message }, status: :unprocessable_entity
       end
+
+      def handle_unauthenticated
+        head :unauthorized
+      end
+
     end
   end
 end
--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,8 +1,18 @@
 class AuthenticationTokenService
-  def self.call
-    hmac_secret = 'my$ecretK3y'
-    payload = { test: "blah" }
+  HMAC_SECRET = 'my$ecretK3y'
+  ALGORITHM_TYPE = 'HS256'
 
-    JWT.encode payload, hmac_secret, 'HS256'
+  def self.call(user_ids)
+    
+    payload = { user_id: user_ids }
+
+    JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
+  end
+
+  def self.decode(token)
+    decoded_token = JWT.decode token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE }
+    decoded_token[0]['user_id']
   end
 end
+
+#decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
