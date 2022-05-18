56559b1047c4a68e5b29bf744010a6136b0df9b3
imubarak234 <imubarak2424@gmail.com>
2022-05-18 04:07:20 +0100

    create user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do 
     namespace :v1 do
-      resources :users,  only: [:index] do
+      resources :users,  only: [:index, :create] do
         resources :reservations, only: [:index, :create, :destroy, :show]
       end
       resources :courses, only: [:index, :create, :destroy, :show] do 
